37df8790d737515b23e43aebe56ecb5407f2d094
zhongxiaoming <zhongxiaomingemail@gmail.com>
2019-06-26 21:52:40 +0800

    demo5

--- godot_demo5/Game.gd
+++ godot_demo5/Game.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 onready var player = $Player
 onready var animationPlayer = $AnimationPlayer
+onready var camera = $Camera2D
 
 var currentAnimation = 'player_start'
 var speed = 200
@@ -14,19 +15,19 @@ func _process(delta):
 	var isMoving = false # 是否按键移动
 	var newAnimation = currentAnimation # 动画变量
 	
-	if Input.is_action_pressed(KEY_LEFT):
+	if Input.is_key_pressed(KEY_LEFT):
 		velocity.x += -1
 		newAnimation = 'player_left'
 		isMoving = true
-	if Input.is_action_pressed(KEY_RIGHT):
+	if Input.is_key_pressed(KEY_RIGHT):
 		velocity.x += 1
 		newAnimation = 'player_right'
 		isMoving = true
-	if Input.is_action_pressed(KEY_UP):
+	if Input.is_key_pressed(KEY_UP):
 		velocity.y += -1
 		newAnimation = 'player_up'
 		isMoving = true
-	if Input.is_action_pressed(KEY_DOWN):
+	if Input.is_key_pressed(KEY_DOWN):
 		velocity.y += 1
 		newAnimation = 'player_down'
 		isMoving = true	
@@ -34,6 +35,7 @@ func _process(delta):
 	if velocity.length() > 0:
 			# 注意这里normalize速度矢量，否则会出现斜着走速度比单方向走速度快
 			player.position += velocity.normalized() * speed * delta
+			updateCameraPosition()
 			
 	updateAnimation(isMoving, newAnimation)		
 
@@ -46,4 +48,5 @@ func updateAnimation(moving, animation):
 		animationPlayer.stop()
 		currentAnimation = 'player_start'
 		
-		
+func updateCameraPosition():
+	camera.position = player.position		
